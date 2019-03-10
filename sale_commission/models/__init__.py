4b10a4ff040389485dd41c11a574db1c97544b72
gastonfeng <gastonfeng@gmail.com>
2019-03-10 17:54:15 +0800

    test project cost

--- sale_commission/models/__init__.py
+++ sale_commission/models/__init__.py
@@ -1,7 +1,8 @@
-from . import account_invoice
+
 from . import product_template
-from . import res_partner
-from . import sale_commission
 from . import sale_commission_mixin
+from . import sale_commission
+from . import res_partner
 from . import sale_order
+from . import account_invoice
 from . import settlement
